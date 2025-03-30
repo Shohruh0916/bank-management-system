48fc369a531ca6762392b9cfd3324f582b3dc1da
Shohruh <sharipovshohruh@gmail.com>
2025-03-30 23:58:03 +0500

    bank-management-system

--- src/bankManagementSystem/version_1/Accounts.java
+++ src/bankManagementSystem/version_1/Accounts.java
@@ -1,5 +1,17 @@
 package bankManagementSystem.version_1;
 
 public class Accounts {
+    private int accountNumber;
+    private String ownerName;
+    private int date;
 
+
+    @Override
+    public String toString() {
+        return "Accounts{" +
+                "accountNumber=" + accountNumber +
+                ", ownerName='" + ownerName + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
--- src/bankManagementSystem/version_1/Bank.java
+++ src/bankManagementSystem/version_1/Bank.java
@@ -2,6 +2,19 @@ package bankManagementSystem.version_1;
 
 public class Bank {
     private String name;
+    private Accounts accounts;
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int createAccount(String ownerName,int date,double deposit){
+        return 0;
+    }
 
 
 }
